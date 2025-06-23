411d479f8bf385c8d77420ec404dde2ba301aee4
arkhamz <68877605+arkhamz@users.noreply.github.com>
2025-06-23 20:28:06 +0200

    update-frontend-origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173', //  frontend domain
+    origin: process.env.FRONTEND_DOMAIN, //  frontend domain
     credentials: true,
   });
 
